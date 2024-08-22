e1edfe062e2ccbf11d13d9d182a744f70226656d
jdnjk <2394058778@qq.com>
2024-08-22 15:11:39 +0800

    Update Flywheel.java

--- common/src/api/java/dev/engine_room/flywheel/api/Flywheel.java
+++ common/src/api/java/dev/engine_room/flywheel/api/Flywheel.java
@@ -1,6 +1,6 @@
 package dev.engine_room.flywheel.api;
 
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.resources.ResourceLocation;
 
 public final class Flywheel {
 	public static final String ID = "flywheel";
@@ -8,7 +8,7 @@ public final class Flywheel {
 	private Flywheel() {
 	}
 
-	public static ResourceLocation resourcelocation(String path) {
-		return new ResourceLocation(ID, path);
+	public static ResourceLocation rl(String path) {
+		return ResourceLocation.fromNamespaceAndPath(ID, path);
 	}
 }
